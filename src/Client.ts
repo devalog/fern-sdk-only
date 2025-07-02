c4e6eaf659a8b0848b9ded3cf6dbf5cf266d2158
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-02 23:50:02 +0000

    Release 2.0.3

--- src/Client.ts
+++ src/Client.ts
@@ -38,8 +38,8 @@ export class DevinApiClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "devin-unique-package",
-                    "X-Fern-SDK-Version": "2.0.2",
-                    "User-Agent": "devin-unique-package/2.0.2",
+                    "X-Fern-SDK-Version": "2.0.3",
+                    "User-Agent": "devin-unique-package/2.0.3",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
